b6e605ca8a7cee620509092437aaacd50c5b8c2b
Timothe <linkazer@gmail.com>
2021-05-17 16:41:31 +0200

    gitflow-feature-stash: Deplacement

---
+++ TreasurePlanetShoot/Assets/Scripts/ShipController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    //On the Scriptable
+    private float acceleration = 0.02f, maxSpeed = .1f;
+
+    [SerializeField]
+    private GameObject canon;
+
+    //On the GO
+    [HideInInspector]
+    public Vector2 shotDirection;
+    private Vector3 moveDirection, inertieDirection, nextPosition;
+    [SerializeField]
+    private Camera cam = default;
+
+    private float currentInertieX, currentInertieY;
+
+    public ShipPlaytimeStatue currentStatue;
+
+    /*private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = currentStatue.baseShip.shipSprite;
+    }*/
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0).normalized;
+
+        currentInertieX = Mathf.Lerp(currentInertieX, moveDirection.x * maxSpeed * 2, acceleration);
+
+        if (currentInertieX < acceleration / 50 && currentInertieX > -acceleration / 50)
+        {
+            currentInertieX = 0;
+        }
+
+        currentInertieY = Mathf.Lerp(currentInertieY, moveDirection.y * maxSpeed * 2, acceleration);
+        if (currentInertieY < acceleration / 50 && currentInertieY > -acceleration / 50)
+        {
+            currentInertieY = 0;
+        }
+
+        inertieDirection = new Vector3(currentInertieX, currentInertieY, 0);
+
+
+        nextPosition = transform.position + inertieDirection;
+        nextPosition = new Vector3(Mathf.Clamp(nextPosition.x, -9, 9), Mathf.Clamp(nextPosition.y, -5, 5), 0);
+
+        transform.position = nextPosition;
+    }
+
+    private void Update()
+    {
+        shotDirection = (cam.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+        if(Vector2.Angle(shotDirection, transform.up) < 75 && Vector2.Angle(shotDirection, transform.up) > -75)
+        {
+            canon.transform.up = shotDirection;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            //currentStatue.TryToShoot();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        inertieDirection /= 10;
+        //currentStatue.TakeDamage(100, 0, true);
+    }
+}
---
+++ TreasurePlanetShoot/Assets/Scripts/ShipPlaytimeStatue.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipPlaytimeStatue : MonoBehaviour
+{
+    /*private float health, maxHealth;
+    public bool isPlayer;
+
+    [HideInInspector]
+    public Vector2 shotDirection;
+
+    public T_EnnemyScriptable baseShip;
+    [HideInInspector]
+    public T_WeaponScriptable weapon;
+
+    private float currentCooldown;
+
+    [SerializeField]
+    private Image healthBar = default;
+
+    private void Start()
+    {
+        ResetValue();
+    }
+
+    public void ResetValue()
+    {
+        maxHealth = baseShip.hitPoints;
+        health = maxHealth;
+        weapon = baseShip.weapon;
+        currentCooldown = weapon.recoveryTime;
+    }
+
+    private void Update()
+    {
+        currentCooldown += Time.deltaTime;
+        shotDirection = transform.up.normalized;
+    }
+
+    public bool TakeDamage(float dmg, float bonusDamage, bool isAlly)
+    {
+        if (isAlly == isPlayer)
+        {
+            health -= dmg + bonusDamage * T_ScoreManager.instance.currentDifficulty;
+            if (health <= 0)
+            {
+                Die();
+            }
+            if (healthBar != null)
+            {
+                healthBar.fillAmount = (maxHealth - health) / maxHealth;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void Die()
+    {
+        if (isPlayer)
+        {
+            T_ScoreManager.instance.EndGame();
+        }
+        else
+        {
+            T_DialogueSimulation.instance.ShowMessage();
+            T_ScoreManager.instance.AddScore(baseShip.score, baseShip.difficultyScore);
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void TryToShoot()
+    {
+        if (currentCooldown >= weapon.recoveryTime)
+        {
+            T_ShootManager.Shoot(this);
+            currentCooldown = 0;
+        }
+    }
+
+    public bool IsCooldownReady()
+    {
+        return (currentCooldown >= weapon.recoveryTime);
+    }*/
+}
