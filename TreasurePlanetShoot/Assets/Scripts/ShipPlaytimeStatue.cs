dcdb112426a4c7fa503f45125dac2d7895857d29
TimotheHome <linkazer@gmail.com>
2021-05-18 15:19:14 +0200

    gitflow-feature-stash: Feedbacks

--- TreasurePlanetShoot/Assets/Scripts/ShipPlaytimeStatue.cs
+++ TreasurePlanetShoot/Assets/Scripts/ShipPlaytimeStatue.cs
@@ -16,7 +16,7 @@ public class ShipPlaytimeStatue : MonoBehaviour
     public Weapon weapon;
 
     private float currentCooldown;
-    private float currentUltiCharge = 1;
+    private float currentUltiCharge = 0;
 
     [SerializeField]
     private Image healthBar, ultiBar;
@@ -52,6 +52,7 @@ public class ShipPlaytimeStatue : MonoBehaviour
             if(isPlayer)
             {
                 invulnerabilityLeft = 2;
+                GetComponent<Animator>().Play("PlayerInvulnerabilityFeedback");
             }
             if (health > 0)
             {
@@ -115,8 +116,12 @@ public class ShipPlaytimeStatue : MonoBehaviour
 
     public void AddUltiCharge(float value)
     {
+        if(currentUltiCharge < 1 && currentUltiCharge+value >= 1)
+        {
+            GetComponent<Animator>().Play("UltiUp");
+        }
         currentUltiCharge += value;
-        if(currentUltiCharge > 1)
+        if(currentUltiCharge >= 1)
         {
             currentUltiCharge = 1;
         }
