9a1f9deb47f053e4624271ef8b182b583525f25d
TimotheHome <linkazer@gmail.com>
2021-05-18 12:39:54 +0200

    Parallaxe

---
+++ TreasurePlanetShoot/Assets/Scripts/ParallaxeBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxeBackground : MonoBehaviour
+{
+    public List<Transform> backgroundSprites;
+    public List<float> speeds;
+
+    private void Update()
+    {
+        for(int i = 0; i < backgroundSprites.Count; i++)
+        {
+            backgroundSprites[i].position -= Vector3.right * speeds[i] * Time.deltaTime;
+            if (backgroundSprites[i].position.x < -25)
+            {
+                backgroundSprites[i].position += Vector3.right * 50;
+            }
+        }
+    }
+}
