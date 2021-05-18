cfd22a31f73b3ae284c92c348c85a01e010a6c5f
TimotheHome <linkazer@gmail.com>
2021-05-18 10:42:02 +0200

    LeUiManager

---
+++ TreasurePlanetShoot/Assets/Scripts/EndingLevelZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingLevelZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<ShipController>() != null)
+        {
+            UIManager.instance.EndLevel();
+        }
+    }
+}
---
+++ TreasurePlanetShoot/Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void EndLevel()
+    {
+
+    }
+}
