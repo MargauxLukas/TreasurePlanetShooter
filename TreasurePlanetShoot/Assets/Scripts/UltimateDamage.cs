bf6b29d3579b2dd6b2169d2d050c81a5ca3c688d
Timothe <linkazer@gmail.com>
2021-05-19 10:48:03 +0200

    Ajout trail player

---
+++ TreasurePlanetShoot/Assets/Scripts/UltimateDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltimateDamage : MonoBehaviour
+{
+    private List<ShipPlaytimeStatue> shipInZone = new List<ShipPlaytimeStatue>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<ShipPlaytimeStatue>() != null)
+        {
+            collision.GetComponent<ShipPlaytimeStatue>().TakeDamage(1, true);
+            shipInZone.Add(collision.GetComponent<ShipPlaytimeStatue>());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<ShipPlaytimeStatue>() != null && shipInZone.Contains(collision.GetComponent<ShipPlaytimeStatue>()))
+        {
+            shipInZone.Add(collision.GetComponent<ShipPlaytimeStatue>());
+        }
+    }
+}
